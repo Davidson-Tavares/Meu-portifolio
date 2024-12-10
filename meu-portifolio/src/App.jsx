ea4640f5b85bafd96778fae884eedbe7c808c9cd
Davidson-Tavares <davidsonandre1@gmail.com>
2024-12-09 23:27:36 -0300

    Update App.jsx

--- meu-portifolio/src/App.jsx
+++ meu-portifolio/src/App.jsx
@@ -1,25 +1,25 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import Styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
-import Experience from './components/Experience/Experience' 
+import Experience from './components/Experience/Experience'
 import Projects from "./components/Projects/Projects"
+import Contact from "./components/Contact/Contact"
 
 function App() {
-  
-  return (
-    
- <div className={Styles.App}>
-  <Navbar/>
-  <Hero />
-  <About />
-  <Experience/>
-  <Projects />
-  
 
- </div>
-    
+  return (
+
+    <div className={Styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      < Contact />
+    </div>
+
   )
 }
 
