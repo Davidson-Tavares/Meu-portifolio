307e9ade5ce1749a2be698648162c6ed220cf988
Davidson-Tavares <davidsonandre1@gmail.com>
2024-12-14 19:52:53 -0300

    ajustes finais

--- meu-portifolio/src/components/Contact/Contact.jsx
+++ meu-portifolio/src/components/Contact/Contact.jsx
@@ -4,24 +4,45 @@ import Styles from "../Contact/Contact.module.css"
 
 import { getImageUrl } from "../../ultis";
 
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { BsThreadsFill } from "react-icons/bs";
+
 const Contact = ()=> {
     return (
         <footer id="contact" className={Styles.container}>
             <div className={Styles.content}>
-            <h2 className={Styles.title}>Contact</h2>
-            <p  className={Styles.description}>Fell free to react out!</p>
+            <h2 className={Styles.title}>Email:</h2>
+            <p  className={Styles.description}>davidsonandre1@gmail.com</p>
             </div>
 
             <div>
                 <ul className={Styles.item}> 
                     <li className={Styles.itens}>
-                        <img src={getImageUrl('contact/email.png')} alt="" /><a href="http://"> myemail.com </a>
+                        <a href="https://www.facebook.com/"> <FaFacebook />  </a>
+
                     </li>
-                    <li>
-                        <img src={getImageUrl('contact/linkedin.png')} alt="" /><a href="http:https://www.linkedin.com/in/davidsontavares/"> myLinkedym.com </a>
+
+                    <li className={Styles.itens}>
+                        <a href="https://github.com/Davidson-Tavares"> <FaGithub /> </a>
+                        
                     </li>
-                    <li>
-                        <img src={getImageUrl('contact/github.png')} alt="" /><a href="http://"> myGitHub.com</a>
+
+                    <li className={Styles.itens}>
+                        <a href="https://www.linkedin.com/in/davidsontavares/"> <FaLinkedin />  </a>
+                        
+                    </li>
+
+                    <li className={Styles.itens}>
+                        <a href="https://www.instagram.com/davidson.andre1/"> <FaInstagram />  </a>
+                        
+                    </li>
+
+                    <li className={Styles.itens}>
+                        <a href="http://"> <BsThreadsFill />  </a>
+                        
                     </li>
                 </ul>
             </div>
--- meu-portifolio/src/components/Hero/Hero.jsx
+++ meu-portifolio/src/components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                     de um curso na Udemy sobre Web Moderno.
                     </p>
 
-                <a href="mailto: davidsonandre1@gmail.com" className={styles.contactBtn}>contact Me</a>
+                <a href="mailto: davidsonandre1@gmail.com" className={styles.contactBtn}>contact Me</a>  
         
             </div>   
                 <img src={getImageUrl('hero/mao-ascenando.png')} alt="Hero image" className={styles.emoji}></img>
